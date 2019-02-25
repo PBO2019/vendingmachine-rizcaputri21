28b6dcb0ab824beabb07937720e4e582711821ca
rizcaputri21 <46665527+rizcaputri21@users.noreply.github.com>
2019-02-25 14:44:32 +0700

    tugas pbo yang benar
    tugas

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Hello World!");
 
         ArrayList <String> a = new ArrayList <String>();
-        Scanner aa = new Scanner (System.in);
+        Scanner keyboard = new Scanner(System.in);
 
         MesinKopi arabica = new MesinKopi();
         arabica.setJenisKopi("Arabica");
@@ -24,29 +24,44 @@ public class Main {
         flores.setJumlahStokKopi(40);
         a.add(flores.getJenisKopi());
 
+        System.out.println("Menu Kopi");
+        for(int i=0; i< a.size();i++){
+            int j= i+1;
+            System.out.println(+j+" "+ a.get(i));
+        }
+
+        System.out.println("Masukan kopi yang mau dipilih ");
+        int pilihanKopi = keyboard.nextInt() -1;
+
+
+        ArrayList <String> b = new ArrayList <String>();
+
         Topping susu = new Topping();
         susu.setJenisTopping("susu");
         susu.setJumlahStokTopping(40);
-        a.add(susu.getJenisTopping());
+        b.add(susu.getJenisTopping());
 
         Topping gula = new Topping();
         gula.setJenisTopping("Gula");
         gula.setJumlahStokTopping(40);
-        a.add(gula.getJenisTopping());
+        b.add(gula.getJenisTopping());
 
         Topping cream = new Topping();
         cream.setJenisTopping("Cream");
         cream.setJumlahStokTopping(40);
-        a.add(cream.getJenisTopping());
+        b.add(cream.getJenisTopping());
 
-        for(int i=0; i< a.size();i++){
+        System.out.println("Menu Topping");
+        for(int i=0; i< b.size();i++){
             int j= i+1;
-            System.out.println("Menu kopi adalah "+j+" "+ a.get(i));
+            System.out.println(j+" "+ b.get(i));
         }
 
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Masukan kopi yang mau dipilih ");
-        int pilihanKopi = keyboard.nextInt() -1;
+
+        System.out.println("Masukan Topping yang mau dipilih ");
+        int pilihantopping = keyboard.nextInt() -1;
+
+        System.out.println("Menu anda Adalah " + a.get(pilihanKopi)+ " dengan Topping "+b.get(pilihantopping));
 
     }
 }
